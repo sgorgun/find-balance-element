addf6bdfdba0fe964acc2d467db2aee960d68e9c
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-09 18:47:29 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ FindBalanceElement/ArrayExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FindBalanceElementTask
+{
+    /// <summary>
+    /// Class for operations with arrays.
+    /// </summary>
+    public static class ArrayExtension
+    {
+        /// <summary>
+        /// Finds an index of element in an integer array for which the sum of the elements
+        /// on the left and the sum of the elements on the right are equal.
+        /// </summary>
+        /// <param name="array">Source array.</param>
+        /// <returns>The index of the balance element, if it exists, and null otherwise.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when source array is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when source array is empty.</exception>
+        public static int? FindBalanceElement(int[]? array)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
